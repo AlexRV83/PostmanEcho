509d53dca6892c9a82fa0c11e5409c4ff7b1a482
AlexRV83 <leksrst@gmail.com>
2021-11-02 00:07:29 +0300

    latest modifications, now test fail

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,6 +1,5 @@
 package ru.netology.postman;
 
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -8,19 +7,21 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 import static org.hamcrest.Matchers.*;
 
 public class PostmanEchoTest {
+
     @Test
-    void ShouldReternTestData(){
+    void shouldPostData() {
+        // Given - When - Then
+        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                // Выполняемые действия
                 .when()
                 .post("/post")
-// Проверки
+                // Проверки
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .contentType(ContentType.JSON)
                 .body("data", equalTo("some data"))
         ;
     }
